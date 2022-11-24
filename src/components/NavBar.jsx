9694564b0a472c552a829b53ccee0a95b8af096d
randricalvin <[randricalvin@gmail.com]>
2022-11-24 21:15:59 +0100

    styling navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,34 +4,22 @@ import logoalefaweb from '../assets/Rgb_black.svg'
 
 const NavBar = () => {
   return (
-    <div className='navbar flex justify-between items-center h-20 px-4 shadow-md fixed w-full bg-white top-0'>
-        <div >
-            <img src={logoalefaweb} alt="logo" className='h-16' />
-        </div>
-        <nav className='flex gap-10'> 
-            <div className='navli flex flex-col w-28'>
-                <Link to="/" className='flex justify-center my-2 link-underline'>
-                    Accueil
+    <div className='navbar flex justify-center items-center'>
+        <nav className=' flex justify-between w-5/6 items-center h-20 text-black relative shadow-sm font-mono' role='navigation'>
+            <div className=''>
+                <Link to='/' className=''>
+                    <img src={logoalefaweb} alt='logo' className='h-14' />
                 </Link>
-                <span className='h-1 bg-blue-700 '></span>
             </div>
-            <div className='navli flex flex-col w-28'>
-                <Link to="/page2" className='flex justify-center my-2'>
-                    Services
-                </Link>
-                <span className='h-1 bg-blue-700'></span>
+            <div className='flex gap-12 '>
+                <Link to='/page1' className=''>Accueil</Link>
+                <Link to='/page2' className=''>Services</Link>
+                <Link to='/page3' className=''>Offres</Link>
+                <Link to='/page4' className=''>Compétences</Link>
+                <Link to='/page5' className=''>Blog</Link>
             </div>
-            <div className='navli flex flex-col w-28'> 
-                <Link to="/page3" className='flex justify-center my-2'>
-                    Offres
-                </Link>
-                <span className='h-1 bg-blue-700'></span>
-            </div>
-            <div className='navli flex flex-col w-28'>
-                <Link to="/page4" className='flex justify-center my-2'>
-                    Contact
-                </Link>
-                <span className='h-1 bg-blue-700'></span>
+            <div className=''>
+                <a href="www.google.com" className='button-contact flex items-center justify-center border-2 border-[#FF7B34] py-2 px-6 rounded-full'>Contact</a>
             </div>
         </nav>
     </div>
